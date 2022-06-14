588e6f96fefa69d0622183736802a921aaa5cc24
bernardo guarana <bernardoguarana1901@gmail.com>
2022-06-14 11:54:05 -0300

    first commit

---
+++ src/config/api-config.js
@@ -0,0 +1,8 @@
+/*
+    Arquivo para mapeamento dos endereços das APIs
+    que serão utilizadas no projeto
+*/
+export const getApiContatos = () => {
+    return 'http://apicontatos-001-site1.btempurl.com/api';
+ }
+ 
